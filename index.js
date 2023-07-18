d17704f7bcca85a0b8323182cbec2e820130e369
Erin Wise <erinwise88@gmail.com>
2023-07-18 10:59:25 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Write your code here //
+/*element = document.getElementById("main");
+
+element.remove();*/
+
+document.getElementById("main").remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+
+newHeader.textContent = "is the champion";
+
