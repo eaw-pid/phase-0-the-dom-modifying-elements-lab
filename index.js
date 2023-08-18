89e5fdc26c7082f2d7a51cd5c9b74fe6931b658b
Erin Wise <erinwise88@gmail.com>
2023-08-17 20:36:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 
 element.remove();*/
 
-document.getElementById("main").remove();
+const main = document.querySelector('main');
+main.remove();
 
 const newHeader = document.createElement('h1');
 
-newHeader.id = "victory";
+newHeader.setAttribute('id', 'victory');
 
-newHeader.textContent = "is the champion";
-
+newHeader.textContent = "ERIN WISE is the champion";
